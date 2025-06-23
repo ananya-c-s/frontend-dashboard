7f66f5bd0bc1d27303d8031a3805b40a29ce5022
Ananya <96562914+ananyacsa@users.noreply.github.com>
2025-06-22 23:22:12 -0400

    first commit

---
+++ src/scenes/global/Topbar.tsx
@@ -0,0 +1,58 @@
+import Box from "@mui/material/Box";
+import { ColorModeContext, tokens } from "../../theme";
+import { useTheme } from "@mui/material/styles";
+import { useContext } from "react";
+import { IconButton, InputBase } from "@mui/material";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import SearchIcon from "@mui/icons-material/Search";
+
+const Topbar = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const colorMode = useContext(ColorModeContext);
+
+  return (
+    <Box
+      component="section"
+      display="flex"
+      justifyContent={"space-between"}
+      p={2}
+    >
+      <Box display="flex" bgcolor={colors.primary[400]} borderRadius="3px">
+        <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
+
+        <IconButton type="button" sx={{ p: 1 }} aria-label="search">
+          <SearchIcon />
+        </IconButton>
+      </Box>
+
+      <Box display="flex">
+        <IconButton onClick={colorMode.toggleColorMode}>
+          {theme.palette.mode === "dark" ? (
+            <LightModeOutlinedIcon />
+          ) : (
+            <DarkModeOutlinedIcon />
+          )}
+        </IconButton>
+        <IconButton>
+          <NotificationsOutlinedIcon />
+        </IconButton>
+        <IconButton>
+          <NotificationsOutlinedIcon />
+        </IconButton>
+        <IconButton>
+          <SettingsOutlinedIcon />
+        </IconButton>
+        <IconButton>
+          <PersonOutlinedIcon />
+        </IconButton>
+      </Box>
+    </Box>
+  );
+};
+
+export default Topbar;
